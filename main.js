6ededc906323dc536333aecdb2369d63089241e0
Xander Luciano <vipercodegames@gmail.com>
2018-04-07 09:16:58 -0700

    Script Cleanup
    Added comments, prevent bot from responding to all bots *properly*

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const winston = require('winston');
 const auth = require('./auth.json');
 
+// Create Logger
 const logger = new (winston.Logger)({
   transports: [
     // colorize the output to the console
@@ -9,23 +10,33 @@ const logger = new (winston.Logger)({
   ]
 });
 
+// Create Discord Client
 const client = new Discord.Client();
 
 let role = null;
 
+// Regex Expressions for commands
 const iamRegex = /(\.iam)+\s(.+)/gi
+const whoisRegex = /(\.whois)+\s(.+)/gi
 const pingRegex = /(\.ping)+\s*/gi
 const pongRegex = /(\.pong)+\s*/gi
-const whoisRegex = /(\.whois)+\s(.+)/gi
 
+// Detects when correctly connected to Discord
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// Detects when a new message is posted in ANY channel
 client.on('message', msg => {
-    if (msg.content[0] !== '.')
+    // Only respond to messages that start with .
+	if (msg.content[0] !== '.')
         return;
     
+	// Don't respond to bots
+    if (msg.author.bot)
+        return;
+    
+	// Display all commands
     if (msg.content === '.help') {
         let commands = [
             '.ping - checks if you are lagging or you are having connection issues',
@@ -44,6 +55,7 @@ client.on('message', msg => {
         msg.channel.send({embed});
     }
 	
+	// Display all roles available at the user level
 	// TODO: Auto generate this list
 	if (msg.content === '.roles') {
 		let roles = [
@@ -69,31 +81,33 @@ client.on('message', msg => {
         msg.channel.send({embed});
 	}
     
+	// Easter Egg
     if (msg.content === '.iam .ping .pong') {
         reply(msg, 'BOT ABUSE!! MODS BOT ABUSE!');
         return;
     }
     
+	// Easter Egg
     if (msg.content === '.iam .ping' || msg.content === '.iam .pong') {
         reply(msg, 'No you\'re not');
         return;
     }
     
+	// Reply to .ping
     if (pingRegex.exec(msg.content)) {
         logger.info(`${msg.author.username}: ${msg.content}`);
         reply(msg, 'pong');
         return;
     }
+	
+	// Easter Egg - Reply to .pong
     if (pongRegex.exec(msg.content)) {
         logger.info(`${msg.author.username}: ${msg.content}`);
         reply(msg, 'that\'s not how this works.');
         return;
     }
     
-    if (msg.author.bot) {
-        return;
-    }
-    
+	// Respond to .whoami with all roles
     if (msg.content === '.whoami') {
         let roles = msg.member.roles.map(r => r.name);
         
@@ -110,8 +124,8 @@ client.on('message', msg => {
         return;
     }
     
+	// Respond to .whois with all users in a role (max 64)
     let roleToFind = whoisRegex.exec(msg.content);
-    
     if (roleToFind) {
         let role = roleToFind[2];
         console.log(role);
@@ -124,13 +138,13 @@ client.on('message', msg => {
         
         let payload = usersInRole.join("\n");
         
-        if (usersInRole > 64) {
-            msg.channel.send('Sorry, too many users to display.');
+        if (usersInRole.length > 64) {
+            msg.channel.send(`Sorry, too many users to display (${usersInRole.length} users).`);
             return;
         }
         
         let embed = new Discord.RichEmbed({
-			"title": `Users with Role ${role}`,
+			"title": `${usersInRole.length} Users with Role `${role}``,
 			"description": payload,
 			"color": 0xFFFF
         });
@@ -140,6 +154,7 @@ client.on('message', msg => {
         return;
     }
     
+	// Respond to .say IF xander said it.
     if (msg.author.id === '250222623923896321') {
         logger.info(`Xander: ${msg.content}`);
         
@@ -155,6 +170,7 @@ client.on('message', msg => {
         }
     }
     
+	// Respond to .iam for adding roles
     let results = iamRegex.exec(msg.content);
     
     if (!results)
@@ -183,6 +199,8 @@ client.on('message', msg => {
             return;
         }
         
+		// Currently a blacklist, should a whitelist.
+		// Should whitelist based on current rank
         if (txtRole === 'user++' || txtRole === 'Rot13' || txtRole === 'Arbiter of Fate'
         || txtRole == 'Our Lord, Jabril' || txtRole === 'The Naughty Corner (Misbehaving)'
         || txtRole === 'Contributor' || txtRole === 'Zapier' || txtRole === '@everyone') {
@@ -191,47 +209,59 @@ client.on('message', msg => {
             return;
         }
         
+		// Should be converted into the whitelist
         if (txtRole !== 'user++' && txtRole !== 'Rot13') {
             user.addRole(role);
             reply(msg, `I've added ${txtRole} to your roles! :smile:`);
+			return;
         }
+		
         return;
     }
 });
 
+// Detects deleted messages
 client.on('messageDelete', (msg) => {
-    if (msg.content === 'ping') {
+    // Detect if user deleted .ping message
+	if (msg.content === '.ping') {
         reply(msg, 'Why u delete ur message to me? :(');
     }
 });
 
+// Reply and log response message
 function reply(msg, text) {
     msg.reply(text);
     logger.info(`RoleX: ${msg.author.username}, ${text}`);
 }
 
-function isVowel(c) {
-    return ['a', 'e', 'i', 'o', 'u'].indexOf(c.toLowerCase()) !== -1
-}
-
+// Converts lower case to Title Case.
+// Improves the chances a user's supplied role matches a valid role
 function toTitleCase(str)
 {
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
+// Replaced with getArticle
+function isVowel(c) {
+    return ['a', 'e', 'i', 'o', 'u'].indexOf(c.toLowerCase()) !== -1
+}
+
+// rigoneri/indefinite-article.js
+// https://github.com/rigoneri/indefinite-article.js
+// Used to respond with a or an correctly
 function getArticle(phrase) {
         
     // Getting the first word 
-    var match = /\w+/.exec(phrase);
+    let match = /\w+/.exec(phrase);
     if (match)
-        var word = match[0];
+        let word = match[0];
     else
         return "an";
     
-    var l_word = word.toLowerCase();
+    let l_word = word.toLowerCase();
     // Specific start of words that should be preceeded by 'an'
-    var alt_cases = ["honest", "hour", "hono"];
-    for (var i in alt_cases) {
+    let alt_cases = ["honest", "hour", "hono"];
+    for (let i in alt_cases) {
         if (l_word.indexOf(alt_cases[i]) == 0)
             return "an";
     }
@@ -251,7 +281,7 @@ function getArticle(phrase) {
     
     // Special cases where a word that begins with a vowel should be preceeded by 'a'
     regexes = [/^e[uw]/, /^onc?e\b/, /^uni([^nmd]|mo)/, /^u[bcfhjkqrst][aeiou]/]
-    for (var i in regexes) {
+    for (let i in regexes) {
         if (l_word.match(regexes[i]))
             return "a"
     }
@@ -278,4 +308,5 @@ function getArticle(phrase) {
     return "a";
 }
 
+// Login to Discord
 client.login(auth.token);
