02956f70b1132c901d0508cad92f876e574b499f
Xander Luciano <vipercodegames@gmail.com>
2018-04-10 02:50:22 -0700

    Update main.js
    Improved logging, improved .whois detection, fixed a few bugs.

--- main.js
+++ main.js
@@ -16,15 +16,42 @@ const client = new Discord.Client();
 let role = null;
 
 // Regex Expressions for commands
-const iamRegex = /(\.iam)+\s(.+)/gi
-const whoisRegex = /(\.whois)+\s(.+)/gi
+const iamRegex = /\.(iam|iama|add|am|is)+\s(.+)/i
+const iamnotRegex = /\.(iamnot|iaint|imnot|remove|iaintno|imno|inot|amnot)+\s(.+)/i
+const whoisRegex = /(\.whois)+\s(.+)/i
 const pingRegex = /(\.ping)+\s*/gi
 const pongRegex = /(\.pong)+\s*/gi
+
+const whitelistRoles = [
+    'composer',
+    'artist',
+    'business',
+    '3d modelling',
+    'game programmer',
+    'q/a',
+    'mobile developer',
+    'web developer',
+    'voice actor',
+    'sound designer',
+    'security',
+];
 
 // Detects when correctly connected to Discord
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.find('name', 'main');
+    
+	// Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+	
+    // Send the message, mentioning the member
+    channel.send(`Welcome to the server, ${member}! Check out <#430981000907194370> and don't forget to <#430970251174215690> to us. :smile:`);
+  });
 
 // Detects when a new message is posted in ANY channel
 client.on('message', msg => {
@@ -38,6 +65,8 @@ client.on('message', msg => {
     
 	// Display all commands
     if (msg.content === '.help') {
+		logger.info(`${msg.author.username} request the .help list.`);
+		
         let commands = [
             '.ping - checks if you are lagging or you are having connection issues',
             '.whoami- retrieve all your current roles',
@@ -58,6 +87,8 @@ client.on('message', msg => {
 	// Display all roles available at the user level
 	// TODO: Auto generate this list
 	if (msg.content === '.roles') {
+		logger.info(`${msg.author.username} request the role list.`);
+		
 		let roles = [
 			'Composer',
 			'Artist',
@@ -111,7 +142,7 @@ client.on('message', msg => {
     if (msg.content === '.whoami') {
         let roles = msg.member.roles.map(r => r.name);
         
-        logger.info(roles);
+        logger.info(`${msg.author.username}'s: ${roles}`);
         
         let embed = new Discord.RichEmbed({
 			"title": `${msg.author.username}'s roles`,
@@ -128,7 +159,15 @@ client.on('message', msg => {
     let roleToFind = whoisRegex.exec(msg.content);
     if (roleToFind) {
         let role = roleToFind[2];
-        console.log(role);
+		
+		// Override for @everyone
+		if (role === "everyone") {
+			role = "@everyone";
+		} else {
+			role = toTitleCase(role);
+		}
+		
+        console.log(`Find users in: ${role}`);
         
         let usersInRole = msg.guild.members.filter(member => { 
             return member.roles.find("name", role);
@@ -138,30 +177,49 @@ client.on('message', msg => {
         
         let payload = usersInRole.join("\n");
         
+		// Don't display more than 64 users (list would be huge in chat)
         if (usersInRole.length > 64) {
-            msg.channel.send(`Sorry, too many users to display (${usersInRole.length} users).`);
+            logger.info(`Too many users (${usersInRole.length}, max: 64)`);
+			msg.channel.send(`Sorry, too many users to display (${usersInRole.length} users).`);
             return;
         }
         
-        let embed = new Discord.RichEmbed({
-			"title": `${usersInRole.length} Users with Role ${role}`,
-			"description": payload,
-			"color": 0xFFFF
-        });
+		// Check if we have users in this Role
+		if (usersInRole.length > 0) {		
+			let embed = new Discord.RichEmbed({
+				"title": `${usersInRole.length} User${usersInRole.length > 1 ? 's are' : ' is'} ${role.replace(/[|&;$%@"<>()+,]/g, '')}`,
+				"description": payload,
+				"color": 0xFFFF
+			});
+			logger.info(usersInRole.join(", "));
+			msg.channel.send({embed});
+			return;
+		}
+		
+		if (usersInRole.length === 0) {
+			logger.error(`No users found in role ${role}`);
+			msg.channel.send(`Sorry, there are no users in ${role.replace(/[|&;$%@"<>()+,]/g, '')}.`);
+			return;
+		}
+		
+        if (!usersusersInRole) {
+			logger.error('Error getting users');
+			msg.channel.send(`Sorry, could not find users in ${role.replace(/[|&;$%@"<>()+,]/g, '')}.`);
+			return;
+		}
         
-        msg.channel.send({embed});
-        
+		
+		logger.error('Error unhandled exception in: whoami');
+		msg.channel.send(`Sorry, I ran into an enexpected error. @Xander - Pls fix.`);
         return;
     }
     
 	// Respond to .say IF xander said it.
     if (msg.author.id === '250222623923896321') {
-        logger.info(`(admin)Xander: ${msg.content}`);
-        
         let regx = /(\.say)+\s(.+)/gi;
         let res = regx.exec(msg.content)
         if(res) {
-            logger.info(res[2]);
+            logger.info(`(admin)Xander: ${res[2]}`);
             msg.channel.send(res[2]);
 			msg.delete();
             return;
@@ -170,54 +228,38 @@ client.on('message', msg => {
     
 	// Respond to .iam for adding roles
     let results = iamRegex.exec(msg.content);
-    
-    if (!results)
-        return;
-    
-    if (results.length != 3)
-        return;
-    
-    if (results[1] === '.iam') {
-        logger.info(`${msg.author.username}: ${results[2]}`);
-        
-        let txtRole = results[2];
-        if (txtRole !== 'user++') {
-            txtRole = toTitleCase(txtRole);
-        }
-		let article = getArticle(results[2]);
-		
-		logger.info(`Requested Role: ${txtRole} or ${results[2]}`);
-        
-        reply(msg, `I understand you claim to be ${article} ${results[2].replace(/[|&;$%@"<>()+,]/g, '')}.`);
-        
-        let user = msg.member;
-        let role = user.guild.roles.find('name', txtRole);
-        
-        if (!role) {
-            logger.error('Invalid role');
-            reply(msg, 'But that doesn\'t seem to be an available role dumbass :neutral_face:');
+    if (results) {
+        if (results.length != 3)
             return;
-        }
         
-		// Currently a blacklist, should a whitelist.
-		// Should whitelist based on current rank
-        if (txtRole === 'user++' || txtRole === 'Rot13' || txtRole === 'Arbiter of Fate'
-        || txtRole == 'Our Lord, Jabril' || txtRole === 'The Naughty Corner (Misbehaving)'
-        || txtRole === 'Contributor' || txtRole === 'Zapier' || txtRole === '@everyone') {
-            logger.error('Protected role');
-            reply(msg, 'How dumb do I look :neutral_face:');
-            return;
+        if (results[1]) {
+            logger.info(`${msg.author.username}: ${results[2]}`);
+            
+            if (iam(msg, results[2]))
+                return;
         }
-        
-		// Should be converted into the whitelist
-        if (txtRole !== 'user++' && txtRole !== 'Rot13') {
-            user.addRole(role);
-            reply(msg, `I've added ${txtRole} to your roles! :smile:`);
-			return;
-        }
-		
+        logger.error('.iam error');
         return;
     }
+    //console.log(results);
+
+    // Respond to .iamnot for removing roles
+    results = iamnotRegex.exec(msg.content);
+    if (results) {
+        if (results.length != 3)
+            return;
+        
+        if (results[1]) {
+            logger.info(`${msg.author.username}: ${results[2]}`);
+
+            if (iamnot(msg, results[2]))
+                return;
+        }
+        logger.error('.iamnot error');
+        return;
+    }
+    console.log(results);
+    logger.warn(`Unknown Command. ${msg.author.username}: '${msg.content}'`);
 });
 
 // Detects deleted messages
@@ -227,6 +269,73 @@ client.on('messageDelete', (msg) => {
         reply(msg, 'Why u delete ur message to me? :(');
     }
 });
+
+function iam(msg, txtRole) {
+    let article = getArticle(txtRole);
+
+    if (txtRole !== 'user++')
+        txtRole = toTitleCase(txtRole);
+    
+    
+    logger.info(`${msg.author.username} Requested Role: '${txtRole}'`);
+    
+    reply(msg, `I understand you claim to be ${article} ${txtRole.replace(/[|&;$%@"<>()+,]/g, '')}.`);
+    
+    let user = msg.member;
+    let role = user.guild.roles.find('name', txtRole);
+    
+    if (!role) {
+        logger.error('Invalid role');
+        reply(msg, 'But that doesn\'t seem to be an available role dumbass :neutral_face:');
+        return true;
+    }
+    
+    // Currently a blacklist, should a whitelist.
+    // Should whitelist based on current rank
+    if (txtRole === 'user++' || txtRole === 'Rot13' || txtRole === 'Arbiter of Fate'
+    || txtRole == 'Our Lord, Jabril' || txtRole === 'The Naughty Corner (Misbehaving)'
+    || txtRole === 'Contributor' || txtRole === 'Zapier' || txtRole === '@everyone') {
+        logger.error('Protected role');
+        reply(msg, 'How dumb do I look :neutral_face:');
+        return true;
+    }
+    
+    // Should be converted into the whitelist
+    if (txtRole !== 'user++' && txtRole !== 'Rot13') {
+        user.addRole(role);
+        reply(msg, `I've added ${txtRole} to your roles! :smile:`);
+        return true;
+    }
+    logger.error('iam error: end of function');
+    return false;
+}
+
+function iamnot(msg, txtRole) {
+    if (txtRole !== 'user++')
+        txtRole = toTitleCase(txtRole);
+
+    logger.info(`${msg.author.username} Requested Role Removal: '${txtRole}'`);
+    
+    let user = msg.member;
+    let role = user.guild.roles.find('name', txtRole);
+
+    if (!role) {
+        logger.error('Invalid role');
+        reply(msg, `${txtRole} doesn\'t seem to be an available role :neutral_face:`);
+        return true;
+    }
+
+    if (whitelistRoles.includes(txtRole.toLowerCase())) {
+        user.removeRole(role);
+        reply(msg, `I've removed ${txtRole} from your roles! :smile:\nEven if you didn't have it :upside_down:`);
+        return true;
+    } else {
+        reply(msg, `I'm sorry Dave, I'm afraid I can't do that.\n${txtRole} is not a whitelisted role.`);
+        return true;
+    }
+    logger.error('iamnot error: end of function');
+    return false;
+}
 
 // Reply and log response message
 function reply(msg, text) {
@@ -238,6 +347,9 @@ function reply(msg, text) {
 // Improves the chances a user's supplied role matches a valid role
 function toTitleCase(str)
 {
+	if (str === "Q/A")
+		return str;
+	
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
@@ -260,14 +372,14 @@ function getArticle(phrase) {
         return "an";
     
     let l_word = word.toLowerCase();
-    // Specific start of words that should be preceeded by 'an'
+    // Specific start of words that should be preceded by 'an'
     let alt_cases = ["honest", "hour", "hono"];
     for (let i in alt_cases) {
         if (l_word.indexOf(alt_cases[i]) == 0)
             return "an";
     }
     
-    // Single letter word which should be preceeded by 'an'
+    // Single letter word which should be preceded by 'an'
     if (l_word.length == 1) {
         if ("aedhilmnorsx".indexOf(l_word) >= 0)
             return "an";
@@ -275,12 +387,12 @@ function getArticle(phrase) {
             return "a";
     }
     
-    // Capital words which should likely be preceeded by 'an'
+    // Capital words which should likely be preceded by 'an'
     if (word.match(/(?!FJO|[HLMNS]Y.|RY[EO]|SQU|(F[LR]?|[HL]|MN?|N|RH?|S[CHKLMNPTVW]?|X(YL)?)[AEIOU])[FHLMNRSX][A-Z]/)) {
         return "an";
     }
     
-    // Special cases where a word that begins with a vowel should be preceeded by 'a'
+    // Special cases where a word that begins with a vowel should be preceded by 'a'
     regexes = [/^e[uw]/, /^onc?e\b/, /^uni([^nmd]|mo)/, /^u[bcfhjkqrst][aeiou]/]
     for (let i in regexes) {
         if (l_word.match(regexes[i]))
@@ -298,11 +410,11 @@ function getArticle(phrase) {
             return "a";
     }
     
-    // Basic method of words that begin with a vowel being preceeded by 'an'
+    // Basic method of words that begin with a vowel being preceded by 'an'
     if ("aeiou".indexOf(l_word[0]) >= 0)
         return "an";
     
-    // Instances where y follwed by specific letters is preceeded by 'an'
+    // Instances where y follwed by specific letters is preceded by 'an'
     if (l_word.match(/^y(b[lor]|cl[ea]|fere|gg|p[ios]|rou|tt)/))
         return "an";
     
